b20b1e7d82b2c081ec43fb810129434bccc5c7dc
chefgonzo <chefgonzo@gmail.com>
2018-08-12 20:45:50 -0400

    started the logic

---
+++ assets/javascript/main.js
@@ -0,0 +1,16 @@
+// Hide the main container till the game starts
+$("#quiz").hide();
+
+// Click on the start game button and begin the game
+$("#start").click(function () {
+    startGame();
+});
+
+// Global Variables
+
+// Start Game Function
+function startGame(){
+    $("#quiz").show();
+
+
+};
